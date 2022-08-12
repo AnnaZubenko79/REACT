9b36e11d605ccca521b68061c4d257d2908fb668
AnnaZubenko79 <annazubenko79@gmail.com>
2022-08-12 20:36:59 +0300

    change avatar

--- lesson3/task3/src/Avatar.jsx
+++ lesson3/task3/src/Avatar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './avatar.scss';
-
 const Avatar = (props) => {
-  return <img className="avatar" src={props.avatarUrl} alt={props.author} />;
+  return <img className="avatar" src={props.avatarUrl} alt={props.name} />;
 };
 export default Avatar;
