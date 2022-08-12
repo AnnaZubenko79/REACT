9389581b6f0f4c11150f6b8480b3997182d8f540
AnnaZubenko79 <annazubenko79@gmail.com>
2022-08-12 20:34:27 +0300

    solve task3 in lesson3

---
+++ lesson3/task3/src/Comment.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './comment.scss';
+import moment from 'moment';
+import UserInfo from './UserInfo';
+
+const formatDate = (date) => moment(date).format('DD MMM YYYY');
+
+function Comment(props) {
+  return (
+    <div className="comment">
+      <div className="comment__text">{props.text}</div>
+      <UserInfo user={props.user} />
+      <div className="comment__date">{formatDate(props.date)}</div>
+    </div>
+  );
+}
+
+export default Comment;
---
+++ lesson3/task3/src/UserInfo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Avatar from './Avatar';
+import './userInfo.scss';
+
+const UserInfo = (props) => {
+  return (
+    <div className="user-info">
+      <Avatar avatarUrl={props.user.avatarUrl} name={props.user.author} />
+      <div className="user-info__name">{props.user.author}</div>
+    </div>
+  );
+};
+
+export default UserInfo;
