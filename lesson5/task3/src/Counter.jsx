c69ab69cbd980c2194b36ab5110c1af9a9968f1f
AnnaZubenko79 <annazubenko79@gmail.com>
2022-08-14 15:21:52 +0300

    change counter.jsx

--- lesson5/task3/src/Counter.jsx
+++ lesson5/task3/src/Counter.jsx
@@ -8,13 +8,11 @@ class Counter extends React.Component {
     };
     this.decrement = this.decrement.bind(this);
   }
-  // привязываем контекст выше в конструкторе с помощью bind
   decrement() {
     this.setState({
       counter: this.state.counter - 1,
     });
   }
-  // контекст привязывается автоматически (самый оптимальный способ)
   increment = () => {
     this.setState({
       counter: this.state.counter + 1,
@@ -35,8 +33,6 @@ class Counter extends React.Component {
         >
           -
         </button>
-        {/* третий способ привязки контекста это свойство стрелочной ф-ции, что не имеет свой контекст и использует внешний контекст. 
-		В нем обязательно вызвать this.reset() */}
         <span className="counter__value" onClick={() => this.reset()}>
           {this.state.counter}
         </span>
