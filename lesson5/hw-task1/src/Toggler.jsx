1f11f51ec5faad91404eb39bc4c98eaa830b925f
AnnaZubenko79 <annazubenko79@gmail.com>
2022-08-14 17:09:27 +0300

    change toggler

--- lesson5/hw-task1/src/Toggler.jsx
+++ lesson5/hw-task1/src/Toggler.jsx
@@ -1,25 +1,45 @@
 import React from 'react';
 
+// class Toggler extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       statusToggler: 'Off',
+//     };
+//   }
+//   handleClick = () => {
+//     this.setState({
+//       statusToggler: 'On',
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <div className="toggler" onToggle={this.handleClick}>
+//         {this.state.statusToggler}
+//       </div>
+//     );
+//   }
+// }
+
 class Toggler extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      statusToggler: 'Off',
-    };
+    this.state = { isToggleOn: true };
   }
+
   handleClick = () => {
-    this.setState({
-      statusToggler: 'On',
-    });
+    this.setState((prevState) => ({
+      isToggleOn: !prevState.isToggleOn,
+    }));
   };
 
   render() {
     return (
-      <div className="toggler" onClick={this.handleClick}>
-        {this.state.statusToggler}
-      </div>
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'Off' : 'On'}
+      </button>
     );
   }
 }
-
 export default Toggler;
