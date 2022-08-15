f101e82056dfa7f9a40d2ce010e5e26dc5b9220d
AnnaZubenko79 <annazubenko79@gmail.com>
2022-08-15 15:33:44 +0300

    create lesson6 and solve task1

---
+++ lesson6/task1/src/Auth.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Greeting from './Greeting';
+import Login from './Login';
+import Logout from './Logout';
+
+class Auth extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
+  onLogin = () => {
+    this.setState({
+      isLoggedIn: true,
+    });
+  };
+  onLogout = () => {
+    this.setState({
+      isLoggedIn: false,
+    });
+  };
+  render() {
+    let button;
+
+    return (
+      <div className="panel">
+        <Greeting isLoggedIn={this.state.isLoggedIn} />
+        {this.state.isLoggedIn ? (
+          <Logout onLogout={this.onLogout} />
+        ) : (
+          <Login onLogin={this.onLogin} />
+        )}
+      </div>
+    );
+  }
+}
+export default Auth;
