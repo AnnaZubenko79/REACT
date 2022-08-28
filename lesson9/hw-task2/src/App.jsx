b23a92d6ed1ae7fc515feb28ff922d9958bb1ba6
AnnaZubenko79 <annazubenko79@gmail.com>
2022-08-28 15:28:56 +0300

    solve hw-task2

--- lesson9/hw-task2/src/App.jsx
+++ lesson9/hw-task2/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import ConnectionStatus from './ConnectionStatus';
+import Game from './Game';
 
 const App = () => {
-  return <ConnectionStatus />;
+  return <Game />;
 };
 
 export default App;
