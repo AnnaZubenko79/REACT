cbd529d51fc9f6da815099ec2d290adde52b0eef
Anna Zubenko <annazubenko79@gmail.com>
2022-09-09 15:38:56 +0300

    fix

--- lesson13/task2/src/App.jsx
+++ lesson13/task2/src/App.jsx
@@ -1,31 +1,60 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
-import Home from './Home';
-import Products from './Products';
+// import React from 'react';
+// import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+// import Home from './Home';
+// import Products from './Products';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="page">
-        <BrowserRouter>
-          <ul className="navigation">
-            <li className="navigation__item">
-              <Link to="/">Home</Link>
-            </li>
-            <li className="navigation__item">
-              <Link to="/products">Products</Link>
-            </li>
-          </ul>
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/products" component={Products} />
-          </Switch>
-        </BrowserRouter>
-      </div>
-    );
-  }
-}
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div className="page">
+//         <BrowserRouter>
+//           <ul className="navigation">
+//             <li className="navigation__item">
+//               <Link to="/">Home</Link>
+//             </li>
+//             <li className="navigation__item">
+//               <Link to="/products">Products</Link>
+//             </li>
+//           </ul>
+//           <Switch>
+//             <Route exact path="/">
+//               <Home />
+//             </Route>
+//             <Route path="/products" component={Products} />
+//           </Switch>
+//         </BrowserRouter>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+import React from 'react';
+import { Link, BrowserRouter, Route } from 'react-router-dom';
+import Products from './Products';
+import Home from './Home';
+
+const App = () => {
+  return (
+    <div className="page">
+      <BrowserRouter>
+        <ul className="navigation">
+          <li className="navigation__item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="navigation__item">
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products" component={Products} />
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
--- lesson13/task2/src/Product.jsx
+++ lesson13/task2/src/Product.jsx
@@ -1,5 +1,14 @@
-import React from 'react';
+// import React from 'react';
 
-const Product = ({ match }) => <div className="product">{match.params.productId}</div>;
+// const Product = ({ match }) => <div className="product">{match.params.productId}</div>;
+
+// export default Product;
+
+import React from 'react';
+// import { useParams } from 'react-router-dom';
+
+const Product = ({ match }) => {
+  return <div className="page__content">{match.params.productId}</div>;
+};
 
 export default Product;
