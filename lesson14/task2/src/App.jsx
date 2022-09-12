c07b40557091a0bf64dba460dd98ea1eb6f4eabe
Anna Zubenko <annazubenko79@gmail.com>
2022-09-12 18:43:30 +0300

    solve task2 in lesson14

--- lesson14/task2/src/App.jsx
+++ lesson14/task2/src/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Route, BrowserRouter, Link, Switch } from 'react-router-dom';
+import User from './User';
 
-class App extends React.Component {
-  render() {
-    return <div className="app">hi</div>;
-  }
-}
+const App = () => (
+  <div className="page">
+    <BrowserRouter>
+      <div className="page__content">
+        <h1>Users</h1>
+        <ul className="navigation">
+          <li className="navigation__item">
+            <Link to="/users/github">Github</Link>
+          </li>
+          <li className="navigation__item">
+            <Link to="/users/facebook">Facebook</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/users/:userId">
+            <User />
+          </Route>
+          <Route path="/">
+            <span>Select a user please</span>
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
